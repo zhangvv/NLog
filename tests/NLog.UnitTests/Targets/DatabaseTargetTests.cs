ef53db835efe7112d8895b3f4ed1dccb5f464947
Julian Verdurmen <304NotModified@users.noreply.github.com>
2016-09-12 22:43:31 +0200

    update header

--- tests/NLog.UnitTests/Targets/DatabaseTargetTests.cs
+++ tests/NLog.UnitTests/Targets/DatabaseTargetTests.cs
@@ -1,5 +1,5 @@
 // 
-// Copyright (c) 2004-2011 Jaroslaw Kowalski <jaak@jkowalski.net>
+// Copyright (c) 2004-2016 Jaroslaw Kowalski <jaak@jkowalski.net>, Kim Christensen, Julian Verdurmen
 // 
 // All rights reserved.
 // 
@@ -29,7 +29,7 @@
 // CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
 // ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
 // THE POSSIBILITY OF SUCH DAMAGE.
-// 
+//
 
 #if !SILVERLIGHT
 
